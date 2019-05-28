45db1f4c889b5f85e8e55edc3672f624b4fbe692
Sakai-san <o_edo@hotmail.com>
2019-05-28 23:21:53 +0200

    Prettier.

--- src/js/app.js
+++ src/js/app.js
@@ -1,48 +1,46 @@
-const memoize = f => {
-  const memo = new Map();
-  return (...args) => {
-    if (memo.get(args[1])) {
-      return memo.get(args[1]);
-    } else {
-      const result = f(args[0], args[1]);
-      memo.set(args[1], result);
-      return result;
-    }
-  };
+const memoize = (f) => {
+    const memo = new Map();
+    return (...args) => {
+        if (memo.get(args[1])) {
+            return memo.get(args[1]);
+        } else {
+            const result = f(args[0], args[1]);
+            memo.set(args[1], result);
+            return result;
+        }
+    };
 };
 
-const sum = numbers => {
-  const sumAux = ([first, ...rest], acc) =>
-    first === undefined ? acc : sumAux(rest, first + acc);
-  return sumAux(numbers, 0);
+const sum = (numbers) => {
+    const sumAux = ([first, ...rest], acc) =>
+        first === undefined ? acc : sumAux(rest, first + acc);
+    return sumAux(numbers, 0);
 };
 
-const avg = grades =>
-  grades.length === 0 ? grades.length : sum(grades) / grades.length;
+const avg = (grades) => (grades.length === 0 ? grades.length : sum(grades) / grades.length);
 
-const getStudent = (students, id) =>
-  students.find(student => student.id === id);
+const getStudent = (students, id) => students.find((student) => student.id === id);
 
 const getStudentAvg = (students, id) => {
-  const student = getStudentMemo(students, id);
-  return avg(student.grades);
+    const student = getStudentMemo(students, id);
+    return avg(student.grades);
 };
 
 const getStudentAvgMemo = memoize(getStudentAvg);
 const getStudentMemo = memoize(getStudent);
 
-const renderStudents = students =>
-  students.map(student =>
-    $(`<li data-student-id=${student.id}>
+const renderStudents = (students) =>
+    students.map((student) =>
+        $(`<li data-student-id=${student.id}>
           <div>${student.first}</div>
           <div>${student.last}</div>
-        </li>`).hover(onHoverHandler(students), () => $("#col2").empty())
-  );
+        </li>`).hover(onHoverHandler(students), () => $('#col2').empty()),
+    );
 
 const renderDetailStudent = (students, studentId) => {
-  const student = getStudentMemo(students, studentId);
-  const studentAvg = getStudentAvgMemo(students, studentId);
-  return $(`
+    const student = getStudentMemo(students, studentId);
+    const studentAvg = getStudentAvgMemo(students, studentId);
+    return $(`
         <div class="student-detail">
           <div>First name : ${student.first}</div>
           <div>Last name : ${student.last}</div>
@@ -50,18 +48,18 @@ const renderDetailStudent = (students, studentId) => {
         </div>`);
 };
 
-const onHoverHandler = students => e => {
-  const listItem = $(e.target).closest("li");
-  if (listItem.length) {
-    const studentId = $(listItem).attr("data-student-id");
-    $("#col2").append(renderDetailStudent(students, parseInt(studentId)));
-  }
+const onHoverHandler = (students) => (e) => {
+    const listItem = $(e.target).closest('li');
+    if (listItem.length) {
+        const studentId = $(listItem).attr('data-student-id');
+        $('#col2').append(renderDetailStudent(students, parseInt(studentId)));
+    }
 };
 
 $(() => {
-  getStudentsFakeApi().then(students => {
-    $("<ul />")
-      .append(renderStudents(students))
-      .appendTo("#col1");
-  });
+    getStudentsFakeApi().then((students) => {
+        $('<ul />')
+            .append(renderStudents(students))
+            .appendTo('#col1');
+    });
 });
